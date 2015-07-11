ec1339fad9a7f617289776ab50639958f8183770
☀ Prem Pillai (@premjg) <prem.pillai@gmail.com>
2015-07-11 22:16:43 +1000

    Fleshes out the SimpleLineBreaker.

--- line_breaker/line_breaker.go
+++ line_breaker/line_breaker.go
@@ -1,8 +1,34 @@
 package line_breaker
 
+import "strings"
+
 type SimpleLineBreaker struct{}
 
-func (s SimpleLineBreaker) GetLines(line string, chars int) []string {
-	lines := []string{"This is an awesome"}
+func (s SimpleLineBreaker) GetLines(text string, maxCharacters int) (lines []string) {
+	words := strings.Split(text, " ")
+	var line string
+
+	for idx, word := range words {
+
+		if wordLength := len(word); wordLength+1 > spaceAvailable(maxCharacters, line) {
+			appendAndFlush(&line, &lines, word)
+		} else {
+			line = line + " " + word
+		}
+
+		if idx == len(words)-1 {
+			appendAndFlush(&line, &lines, word)
+		}
+	}
+
 	return lines
+}
+
+func appendAndFlush(line *string, lines *[]string, word string) {
+	*lines = append(*lines, strings.Trim(*line, " "))
+	*line = word
+}
+
+func spaceAvailable(maxCharacters int, line string) int {
+	return (maxCharacters - len(strings.Trim(line, " ")))
 }
